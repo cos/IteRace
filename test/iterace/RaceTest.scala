0e245351433d09ad6a8f81eb3b4995dac5cc705b
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-03-09 17:41:22 -0600

    extracted general test class

--- test/iterace/RaceTest.scala
+++ test/iterace/RaceTest.scala
@@ -1,5 +1,40 @@
 package iterace
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
 
-abstract class RaceTest {
+import iterace.WALAConversions._
+import iterace.LoopContextSelector.LoopCallSiteContext
+import org.junit.Assert._
+import scala.collection._
 
+abstract class RaceTest extends FunSuite with BeforeAndAfter  {
+  val dependencies = List("particles", "../lib/parallelArray.mock")
+  val startClass = "Lparticles/Particle"
+
+  def analyze(method: String) = new IteRace(startClass, method, dependencies)
+
+  def printRaces(races: Map[N, Map[O, Map[F, RSet]]]): String = {
+    val s = new StringBuilder
+    s ++= "\n"
+    for ((l, lr) <- races) {
+      s ++= "Loop: "+l.getContext().asInstanceOf[LoopCallSiteContext].prettyPrint() + "\n\n"
+      for ((o, fr) <- lr) {
+        s ++= o.prettyPrint() + "\n"
+        for ((f, rr) <- fr) {
+          s ++= " ." + f.getName() + "\n"
+          s ++= rr.prettyPrint() +"\n"
+        }
+      }
+    }
+    s.toString()
+  }
+  
+  def testResult(method: String, result: String) = {
+    test(method) {
+      val iterace = analyze(method+"()V")
+      assertEquals(result, printRaces(iterace.races))
+    }
+  }
+  
+  def testNoRaces(method: String) = testResult(method, "\n")
 }
--- test/iterace/TestParticle.scala
+++ test/iterace/TestParticle.scala
@@ -7,42 +7,12 @@ import scala.collection.JavaConversions._
 import iterace.WALAConversions._
 import org.junit.Assert._
 import iterace.LoopContextSelector.LoopCallSiteContext
-import scala.collection.mutable._
+import scala.collection._
 import org.scalatest.FunSuite
 import org.junit.Rule
 
 @RunWith(classOf[JUnitRunner])
-class TestParticle extends FunSuite with BeforeAndAfter {
-
-  val dependencies = List("particles", "../lib/parallelArray.mock")
-  val startClass = "Lparticles/Particle"
-
-  def analyze(method: String) = new IteRace(startClass, method, dependencies)
-
-  def printRaces(races: Map[N, Map[O, Map[F, RSet]]]): String = {
-    val s = new StringBuilder
-    s ++= "\n"
-    for ((l, lr) <- races) {
-      s ++= "Loop: "+l.getContext().asInstanceOf[LoopCallSiteContext].prettyPrint() + "\n\n"
-      for ((o, fr) <- lr) {
-        s ++= o.prettyPrint() + "\n"
-        for ((f, rr) <- fr) {
-          s ++= " ." + f.getName() + "\n"
-          s ++= rr.prettyPrint() +"\n"
-        }
-      }
-    }
-    s.toString()
-  }
-  
-  def testResult(method: String, result: String) = {
-    test(method) {
-      val iterace = analyze(method+"()V")
-      assertEquals(result, printRaces(iterace.races))
-    }
-  }
-  
-  def testNoRaces(method: String) = testResult(method, "\n")
+class TestParticle extends RaceTest {
   
   testNoRaces("vacuouslyNoRace")
   
