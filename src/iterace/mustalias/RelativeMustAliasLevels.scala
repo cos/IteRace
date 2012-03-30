dc0658021ce9befca52c85b846ee95ad66a73891
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-03-30 18:18:44 -0500

    just keeping things compiling

--- src/iterace/mustalias/RelativeMustAliasLevels.scala
+++ src/iterace/mustalias/RelativeMustAliasLevels.scala
@@ -10,28 +10,28 @@ import com.ibm.wala.ssa.SSAPutInstruction
 
 class RelativeMustAliasLevels(pa: PointerAnalysis) {
   import pa._
-
-  class MA {
-    val ps = Set.empty[PP]
-  	val parentSet = Set.empty[PP]
-    var fieldToParent: F = null
-    
-    def add(p: PP): Boolean = {
-      val i = p.getDef()
-      if(i == null) 
-        false
-      else i match {
-        case i: SSAGetInstruction => {
-          if(fieldToParent == null) fieldToParent = i.getDeclaredField() // for the initial set
-          
-          if(i.getDeclaredField() != fieldToParent) return false
-          
-          parentSet.add(PP(p.n, i.getRef()))
-          ps.add(p)
-        }
-      }
-    }
-  }
+//
+//  class MA {
+//    val ps = Set.empty[PP]
+//  	val parentSet = Set.empty[PP]
+//    var fieldToParent: F = null
+//    
+//    def add(p: PP): Boolean = {
+//      val i = p.getDef()
+//      if(i == null) 
+//        false
+//      else i match {
+//        case i: SSAGetInstruction => {
+//          if(fieldToParent == null) fieldToParent = i.getDeclaredField() // for the initial set
+//          
+//          if(i.getDeclaredField() != fieldToParent) return false
+//          
+//          parentSet.add(PP(p.n, i.getRef()))
+//          ps.add(p)
+//        }
+//      }
+//    }
+//  }
 
   def mustAlias(n: N)(q1: P, q2: P): Boolean = {
     false
