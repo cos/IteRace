ff6cf4a5b42140e17538a198de1810a5a03bb0ef
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-04-06 18:57:48 -0500

    removed some unnecessary test classes

--- test/iterace/TestLockSetNow.scala
+++
@@ -1,8 +0,0 @@
-//package iterace
-//import org.junit.runner.RunWith
-//import org.scalatest.junit.JUnitRunner
-//
-////@RunWith(classOf[JUnitRunner])
-////class TestLocksNow extends LockSetTest(List("particles", "../lib/parallelArray.mock"), "Lparticles/ParticleWithLocks") {
-////	testGetLockSet("checkMeetOverAllValidPathsNegative", "xyz", "{  }")
-////}
