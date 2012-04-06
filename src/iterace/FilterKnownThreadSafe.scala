9b704156746bd4d151c051f26a440c35723acf2f
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-04-06 11:52:40 -0500

    fixed small bug in last change

--- src/iterace/FilterKnownThreadSafe.scala
+++ src/iterace/FilterKnownThreadSafe.scala
@@ -35,20 +35,20 @@ class FilterByKnownThreadSafe extends Function1[Set[Race], Set[Race]] {
 object threadSafe {
   val threadSafe = Seq[String](
     // used for ignoring string operations, including concatenation
-    "Ljava/lang/String, ",
-    "Ljava/lang/StringBuilder, ",
-    "Ljava/lang/StringBuffer, ",
+    "Ljava/lang/String",
+    "Ljava/lang/StringBuilder",
+    "Ljava/lang/StringBuffer",
     // used for ignoring System.out stuff. but it is thread-safe in general
-    "Ljava/io/PrintStream, ",
-    "Ljava/lang/Throwable, ",
+    "Ljava/io/PrintStream",
+    "Ljava/lang/Throwable",
 
     // kind of coarse-grained
-    "Ljava/lang/System, ")
+    "Ljava/lang/System")
 
   def apply(m: IMethod): Boolean = {
     val isIt = threadSafe.exists { m.getDeclaringClass().getName().toString().equals(_) }
     
-//    if (m.toString().contains("initializeSystemClass")) println(m.toString() + isIt);
+//    if (m.toString().contains("Throwable")) println(m.toString() + isIt);
     isIt
   }
 
