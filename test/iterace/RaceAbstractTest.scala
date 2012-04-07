dfa57a6ff1374cae59ffcb69b2762a75e5d443bc
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-04-06 19:06:29 -0500

    added "focus" mechanism to test to get around the fact eclipse doesn't know how to run an individual test

--- test/iterace/RaceAbstractTest.scala
+++ test/iterace/RaceAbstractTest.scala
@@ -12,44 +12,49 @@ import com.ibm.wala.properties.WalaProperties
 import iterace.oldjava.AnalysisScopeBuilder
 
 abstract class RaceAbstractTest(startClass: String) extends FunSuite with BeforeAndAfter {
-	val analysisScope = new AnalysisScopeBuilder("/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar");
-	analysisScope.setExclusionsFile("walaExclusions.txt");
-	analysisScope.addBinaryDependency("../lib/parallelArray.mock");
-	
-	val stages: Seq[Stage] = Seq(FilterByLockMayAlias, BubbleUpToAppLevel, FilterByLockMayAlias);
-	
+  val analysisScope = new AnalysisScopeBuilder("/System/Library/Frameworks/JavaVM.framework/Classes/classes.jar");
+  analysisScope.setExclusionsFile("walaExclusions.txt");
+  analysisScope.addBinaryDependency("../lib/parallelArray.mock");
+
+  val stages: Seq[Stage] = Seq(FilterByLockMayAlias, BubbleUpToAppLevel, FilterByLockMayAlias);
+
+  var focusOnMethod: Option[String] = None
+  def focus(method: String) = focusOnMethod = Some(method)
+
   def analyze(method: String) = {
-  	log("test: "+method)
+    log("test: " + method)
     IteRace(startClass, method, analysisScope, stages)
   }
 
   def printRaces(races: ProgramRaceSet): String = "\n" + races.prettyPrint + "\n"
- 
-      
-//    for ((l, lRaces) <- races.groupBy { _.l } toStringSorted) {
-//      s ++= "Loop: " + l.n.getContext().asInstanceOf[LoopCallSiteContext].prettyPrint() + "\n\n"
-//      for ((o, oRaces) <- lRaces.groupBy { _.o } toStringSorted) {
-//        s ++= o.prettyPrint() + "\n"
-//        val racesOnFields = oRaces collect { case r: RaceOnField => r }
-//        for ((f, fRaces) <- racesOnFields.groupBy { _.f } toStringSorted) {
-//          s ++= ((new FieldRaceSet(f, fRaces)).prettyPrint) + "\n"
-//        }
-//
-//        val shallowRaces = oRaces collect {case r: ShallowRace => r}
-//        if(!shallowRaces.isEmpty)
-//        s ++= " on object: \n" + new ShallowRaceSet(shallowRaces).prettyPrint() + "\n"
-//        
-//      }
-//    }
+
+  //    for ((l, lRaces) <- races.groupBy { _.l } toStringSorted) {
+  //      s ++= "Loop: " + l.n.getContext().asInstanceOf[LoopCallSiteContext].prettyPrint() + "\n\n"
+  //      for ((o, oRaces) <- lRaces.groupBy { _.o } toStringSorted) {
+  //        s ++= o.prettyPrint() + "\n"
+  //        val racesOnFields = oRaces collect { case r: RaceOnField => r }
+  //        for ((f, fRaces) <- racesOnFields.groupBy { _.f } toStringSorted) {
+  //          s ++= ((new FieldRaceSet(f, fRaces)).prettyPrint) + "\n"
+  //        }
+  //
+  //        val shallowRaces = oRaces collect {case r: ShallowRace => r}
+  //        if(!shallowRaces.isEmpty)
+  //        s ++= " on object: \n" + new ShallowRaceSet(shallowRaces).prettyPrint() + "\n"
+  //        
+  //      }
+  //    }
 
   def result(iteRace: IteRace): ProgramRaceSet = iteRace.races
- 
-  def testResult(method: String, expectedResult: String) = {
-    test(method) {
-      val iterace = analyze(method + "()V")
-      assertEquals(expectedResult, printRaces(result(iterace)))
+
+  def testResult(method: String, expectedResult: String): Unit = {
+    focusOnMethod match {
+      case Some(m) if m != method => // do nothing
+      case _ => test(method) {
+        val iterace = analyze(method + "()V")
+        assertEquals(expectedResult, printRaces(result(iterace)))
+      }
     }
   }
 
-  def testNoRaces(method: String) = testResult(method, "\n\n")
+  def testNoRaces(method: String):Unit = testResult(method, "\n\n")
 }
