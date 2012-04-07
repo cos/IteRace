5af1421c2b326ca37889db22e335c176f7b0eb67
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-04-07 18:10:48 -0500

    adapted the filterByMayAlias phase to use the new lock analysis

--- src/iterace/stage/FilterByLockMayAlias.scala
+++ src/iterace/stage/FilterByLockMayAlias.scala
@@ -17,8 +17,7 @@ class FilterByLockMayAlias(pa: RacePointerAnalysis) extends Stage {
   def apply(races: ProgramRaceSet): ProgramRaceSet = {
     new ProgramRaceSet(races.children map (loopRaceSet => {
       val locks = lockSet.getLocks(loopRaceSet.l)
-      val locksWithUniqueAbstractObjects = locks.filter({ pointsToUniqueAbstractObject(_) })
-      val lockMap = lockSet.getLockSetMapping(loopRaceSet.l, locksWithUniqueAbstractObjects)
+      val lockMap = lockSet.getLockSetMapping(loopRaceSet.l, locks)
 
       def isSafe(r: Race): Boolean = {
         val aLocks = lockMap(r.a)
@@ -28,26 +27,6 @@ class FilterByLockMayAlias(pa: RacePointerAnalysis) extends Stage {
       }
       loopRaceSet.filter { !isSafe(_) }
     }))
-    //    races groupBy { _.l } collect {
-    //      case (loop, potentialRaceSet) => {
-    //        val locks = lockSet.getLocks(loop)
-    //        val locksWithUniqueAbstractObjects = locks.filter({ pointsToUniqueAbstractObject(_) })
-    //        val lockMap = lockSet.getLockSetMapping(loop, locksWithUniqueAbstractObjects)
-    //
-    //        def isSafe(r: Race): Boolean = {
-    //          val lockObjectsA = lockMap(r.a) map { _.p.pt } flatten
-    //          val lockObjectsB = lockMap(r.b) map { _.p.pt } flatten
-    //
-    //          lockObjectsA.size == 1 && lockObjectsB.size == 1 && (lockObjectsA & lockObjectsB).size == 1
-    //        }
-    //
-    //        potentialRaceSet filter { !isSafe(_) }
-    //      }
-  }
-
-  def pointsToUniqueAbstractObject(l: Lock): Boolean = {
-//    l.p.pt.size == 1
-    true
   }
 }
 
