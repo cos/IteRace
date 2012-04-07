56e63a1012c78a9cb4b5a760597b704bf91765eb
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-04-07 11:32:40 -0500

    fixed a bug with the context interpreter - for some reason WALA needs a configuration like default -> reflexion -> default instead of reflexion -> default... anyway

--- src/iterace/pointeranalysis/CallGraphBuilder.scala
+++ src/iterace/pointeranalysis/CallGraphBuilder.scala
@@ -5,7 +5,7 @@ import com.ibm.wala.ipa.callgraph.AnalysisCache;
 import com.ibm.wala.ipa.callgraph.AnalysisOptions;
 import com.ibm.wala.ipa.callgraph.AnalysisScope;
 import com.ibm.wala.ipa.callgraph.ContextSelector;
-import com.ibm.wala.ipa.callgraph.propagation.{SSAContextInterpreter,SSAPropagationCallGraphBuilder,InstanceKeyFactory}
+import com.ibm.wala.ipa.callgraph.propagation.{ SSAContextInterpreter, SSAPropagationCallGraphBuilder, InstanceKeyFactory }
 import com.ibm.wala.ipa.callgraph.propagation.cfa.DefaultPointerKeyFactory;
 import com.ibm.wala.ipa.callgraph.propagation.cfa.DefaultSSAInterpreter;
 import com.ibm.wala.ipa.callgraph.propagation.cfa.DelegatingSSAContextInterpreter;
@@ -15,28 +15,32 @@ import com.ibm.wala.ipa.cha.IClassHierarchy;
 import com.ibm.wala.ipa.callgraph.impl.Util;
 
 class CallGraphBuilder(
-    options: AnalysisOptions,
-    cache: AnalysisCache,
-	  cha: IClassHierarchy ,
-	  scope: AnalysisScope)
-	  extends SSAPropagationCallGraphBuilder(cha, options, cache, new DefaultPointerKeyFactory()) {
-  
-		Util.addDefaultSelectors(options, cha);
-		Util.addDefaultBypassLogic(options, scope, classOf[Util].getClassLoader(), cha);
-		
-		val instancePolicy =  
-		  		ZeroXInstanceKeys.SMUSH_STRINGS | 
-		  		ZeroXInstanceKeys.ALLOCATIONS | 
-		  		ZeroXInstanceKeys.SMUSH_THROWABLES
-//		  		ZeroXInstanceKeys.SMUSH_MANY |
-		
-		val contextInterpreter = new DelegatingSSAContextInterpreter(
-        ReflectionContextInterpreter.createReflectionContextInterpreter(cha, options, cache),  
-        new DefaultSSAInterpreter(options, cache))
-		  		
-    setContextInterpreter(contextInterpreter)
-    
-    val zik: ZeroXInstanceKeys = new ZeroXInstanceKeys(options, cha, contextInterpreter, instancePolicy)
-    setContextSelector(new LoopContextSelector(Set(), zik))
-    setInstanceKeys(zik);
+  options: AnalysisOptions,
+  cache: AnalysisCache,
+  cha: IClassHierarchy,
+  scope: AnalysisScope)
+  extends SSAPropagationCallGraphBuilder(cha, options, cache, new DefaultPointerKeyFactory()) {
+
+  Util.addDefaultSelectors(options, cha);
+  Util.addDefaultBypassLogic(options, scope, classOf[Util].getClassLoader(), cha);
+
+  val instancePolicy =
+    ZeroXInstanceKeys.SMUSH_STRINGS |
+      ZeroXInstanceKeys.ALLOCATIONS |
+      ZeroXInstanceKeys.SMUSH_THROWABLES
+  //		  		ZeroXInstanceKeys.SMUSH_MANY |
+
+  val defaultInterpreter = new DefaultSSAInterpreter(options, cache)
+
+  val reflectionInterpreter = new DelegatingSSAContextInterpreter(
+    ReflectionContextInterpreter.createReflectionContextInterpreter(cha, options, cache), defaultInterpreter)
+
+  val contextInterpreter = new DelegatingSSAContextInterpreter(
+    defaultInterpreter, reflectionInterpreter)
+
+  this.setContextInterpreter(contextInterpreter)
+
+  val zik: ZeroXInstanceKeys = new ZeroXInstanceKeys(options, cha, contextInterpreter, instancePolicy)
+  setContextSelector(new LoopContextSelector(Set(), zik))
+  setInstanceKeys(zik);
 }
