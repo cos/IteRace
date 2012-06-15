01c8742d3103f903bc817a9ce4b1047b4ebaa0cd
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-06-15 12:17:28 -0500

    small change - just formatted a file

--- src/iterace/util/ArrayContents.java
+++ src/iterace/util/ArrayContents.java
@@ -10,86 +10,86 @@ import com.ibm.wala.util.debug.UnimplementedError;
 import com.ibm.wala.util.strings.Atom;
 
 /***
- *
+ * 
  * Derived from ArrayContents in demandpa package
- *
+ * 
  */
 
 public class ArrayContents implements IField {
 
-  private static final ArrayContents theContents = new ArrayContents();
+	public static final ArrayContents theContents = new ArrayContents();
 
-  public static final ArrayContents v() {
-    return theContents;
-  }
+	public static final ArrayContents v() {
+		return theContents;
+	}
 
-  private ArrayContents() {
-  }
+	private ArrayContents() {
+	}
 
-  @Override
-public TypeReference getFieldTypeReference() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return null;
-  }
+	@Override
+	public TypeReference getFieldTypeReference() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return null;
+	}
 
-  @Override
-public boolean isFinal() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return false;
-  }
+	@Override
+	public boolean isFinal() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return false;
+	}
 
-  @Override
-public boolean isPrivate() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return false;
-  }
+	@Override
+	public boolean isPrivate() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return false;
+	}
 
-  @Override
-public boolean isProtected() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return false;
-  }
+	@Override
+	public boolean isProtected() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return false;
+	}
 
-  @Override
-public boolean isPublic() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return false;
-  }
+	@Override
+	public boolean isPublic() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return false;
+	}
 
-  @Override
-public boolean isStatic() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return false;
-  }
+	@Override
+	public boolean isStatic() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return false;
+	}
 
-  @Override
-public IClass getDeclaringClass() throws UnsupportedOperationException {
-    throw new UnsupportedOperationException();
-  }
+	@Override
+	public IClass getDeclaringClass() throws UnsupportedOperationException {
+		throw new UnsupportedOperationException();
+	}
 
-  @Override
-public Atom getName() throws UnimplementedError {
-    return Atom.findOrCreateUnicodeAtom("[*]");
-  }
+	@Override
+	public Atom getName() throws UnimplementedError {
+		return Atom.findOrCreateUnicodeAtom("[*]");
+	}
 
-  @Override
-  public String toString() {
-    return "[*]";
-  }
+	@Override
+	public String toString() {
+		return "[*]";
+	}
 
-  @Override
-public boolean isVolatile() {
-    return false;
-  }
+	@Override
+	public boolean isVolatile() {
+		return false;
+	}
 
-  @Override
-public ClassHierarchy getClassHierarchy() throws UnimplementedError {
-    Assertions.UNREACHABLE();
-    return null;
-  }
+	@Override
+	public ClassHierarchy getClassHierarchy() throws UnimplementedError {
+		Assertions.UNREACHABLE();
+		return null;
+	}
 
-  @Override
-public FieldReference getReference() {
-    return null;
-  }
+	@Override
+	public FieldReference getReference() {
+		return null;
+	}
 }
