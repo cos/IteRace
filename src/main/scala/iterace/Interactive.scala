33767345ab6e4485b3e5a75b98077b4462e19fd2
Cosmin Radoi <cosmin.radoi@gmail.com>
2013-02-23 18:13:52 -0600

    comment out the Tracer and Interactive parts to make it compile with the heavily refactored WALAFacade

--- src/main/scala/iterace/Interactive.scala
+++ src/main/scala/iterace/Interactive.scala
@@ -1,84 +1,84 @@
-package iterace
-
-import iterace.util.Tracer
-import edu.illinois.wala.S
-import edu.illinois.wala.Facade._
-import scala.collection.JavaConverters._
-import com.ibm.wala.ipa.callgraph.propagation.AbstractFieldPointerKey
-import com.ibm.wala.ipa.callgraph.propagation.PointerKey
-import com.ibm.wala.ipa.callgraph.propagation.LocalPointerKey
-
-object Interactive extends App {
-  val r = new IteRaceRunner(args.toList)
-  val iteRace = r run
-
-  val t = new Tracer(iteRace.pa.getCallGraph(), iteRace.pa.getPointerAnalysis())
-
-  var l = ""
-  object IsInt {
-    def unapply(s: String): Option[Int] = try {
-      Some(s.toInt)
-    } catch {
-      case _: java.lang.NumberFormatException => None
-    }
-  }
-
-  val intToAccess = S.accessesRepo map { _.swap }
-
-  do {
-    print("\n>")
-    l = readLine
-    l match {
-      case "s" => println("\n" + scala.io.Source.fromFile(r.config.getString("iterace.races-file")).mkString) // show races
-      case "q" => // quit
-      case IsInt(n) => {
-        val s = intToAccess(n)
-        println(s.prettyPrint)
-        t.trace(s)
-      }
-      case s: String if s.startsWith("list-vars") => { // list the variables in a cg node
-//        val name = s.split(" ").tail mkString " "
-//        (iteRace.pa.cg.asScala.find(_.toString.contains(name))) match {
-//          case Some(n) => {
-//            println(n.toString)
-//            Range(1, n.getIR().getSymbolTable().getMaxValueNumber()) foreach { x =>
-//              println(iteRace.pa.heap.asScala.find(_ == P(n, x))
-//                map { pk => x + " : \n" + (iteRace.pa.heap.getSuccNodes(pk).asScala map { _.asInstanceOf[O].prettyPrint } mkString "\n") }
-//                mkString "\n")
-//            }
-//          }
-//          case None => println("node not found");
-//        }
-      }
-      case "o" => {
-//        val intToObjects = O.printRepo map { _.swap }
-//        val x = readLine.toInt
-//        println(intToObjects(x).prettyPrint)
-      }
-      case s: String if s.startsWith("opp") => {
+//package iterace
+//
+//import iterace.util.Tracer
+//import edu.illinois.wala.S
+//import edu.illinois.wala.Facade._
+//import scala.collection.JavaConverters._
+//import com.ibm.wala.ipa.callgraph.propagation.AbstractFieldPointerKey
+//import com.ibm.wala.ipa.callgraph.propagation.PointerKey
+//import com.ibm.wala.ipa.callgraph.propagation.LocalPointerKey
+//
+//object Interactive extends App {
+//  val r = new IteRaceRunner(args.toList)
+//  val iteRace = r run
+//
+//  val t = new Tracer(iteRace.pa.getCallGraph(), iteRace.pa.getPointerAnalysis())
+//
+//  var l = ""
+//  object IsInt {
+//    def unapply(s: String): Option[Int] = try {
+//      Some(s.toInt)
+//    } catch {
+//      case _: java.lang.NumberFormatException => None
+//    }
+//  }
+//
+//  val intToAccess = S.accessesRepo map { _.swap }
+//
+//  do {
+//    print("\n>")
+//    l = readLine
+//    l match {
+//      case "s" => println("\n" + scala.io.Source.fromFile(r.config.getString("iterace.races-file")).mkString) // show races
+//      case "q" => // quit
+//      case IsInt(n) => {
+//        val s = intToAccess(n)
+//        println(s.prettyPrint)
+//        t.trace(s)
+//      }
+//      case s: String if s.startsWith("list-vars") => { // list the variables in a cg node
+////        val name = s.split(" ").tail mkString " "
+////        (iteRace.pa.cg.asScala.find(_.toString.contains(name))) match {
+////          case Some(n) => {
+////            println(n.toString)
+////            Range(1, n.getIR().getSymbolTable().getMaxValueNumber()) foreach { x =>
+////              println(iteRace.pa.heap.asScala.find(_ == P(n, x))
+////                map { pk => x + " : \n" + (iteRace.pa.heap.getSuccNodes(pk).asScala map { _.asInstanceOf[O].prettyPrint } mkString "\n") }
+////                mkString "\n")
+////            }
+////          }
+////          case None => println("node not found");
+////        }
+//      }
+//      case "o" => {
+////        val intToObjects = O.printRepo map { _.swap }
+////        val x = readLine.toInt
+////        println(intToObjects(x).prettyPrint)
+//      }
+//      case s: String if s.startsWith("opp") => {
+////        val x = s.split(" ")(1).toInt
+////        val intToObjects = O.printRepo map { _.swap }
+////        val heap = iteRace.pa.heap
+////        heap.getPredNodes(intToObjects(x)).asScala flatMap { heap.getPredNodes(_).asScala } foreach {
+////          case o: O => println(o.prettyPrint)
+////          case p => println(p.toString)
+////        }
+//      }
+//      case s: String if s.startsWith("op") => {
 //        val x = s.split(" ")(1).toInt
-//        val intToObjects = O.printRepo map { _.swap }
-//        val heap = iteRace.pa.heap
-//        heap.getPredNodes(intToObjects(x)).asScala flatMap { heap.getPredNodes(_).asScala } foreach {
-//          case o: O => println(o.prettyPrint)
-//          case p => println(p.toString)
-//        }
-      }
-      case s: String if s.startsWith("op") => {
-        val x = s.split(" ")(1).toInt
-//        val intToObjects = O.printRepo map { _.swap }
-//        val heap = iteRace.pa.heap
-//        heap.getPredNodes(intToObjects(x)).asScala foreach {
-//          case o: O => println(o.prettyPrint)
-//          case o: P =>
-//          case p: PointerKey => println(p.prettyPrint)
-//        }
-      }
-      case s: String if s.startsWith("cha") => println(iteRace.pa.cha.asScala.find({ case c => c.toString.contains(s.substring(4)) }))
-      case s: String if s.startsWith("cg") => println(iteRace.pa.cg.asScala.filter({ case c => c.toString.contains(s.substring(3)) }) mkString "\n")
-      case _ => println("unknown command")
-    }
-  } while (l != "q")
-
-  println("bye!")
-}
+////        val intToObjects = O.printRepo map { _.swap }
+////        val heap = iteRace.pa.heap
+////        heap.getPredNodes(intToObjects(x)).asScala foreach {
+////          case o: O => println(o.prettyPrint)
+////          case o: P =>
+////          case p: PointerKey => println(p.prettyPrint)
+////        }
+//      }
+//      case s: String if s.startsWith("cha") => println(iteRace.pa.cha.asScala.find({ case c => c.toString.contains(s.substring(4)) }))
+//      case s: String if s.startsWith("cg") => println(iteRace.pa.cg.asScala.filter({ case c => c.toString.contains(s.substring(3)) }) mkString "\n")
+//      case _ => println("unknown command")
+//    }
+//  } while (l != "q")
+//
+//  println("bye!")
+//}
--- src/main/scala/iterace/util/Tracer.scala
+++ src/main/scala/iterace/util/Tracer.scala
@@ -1,114 +1,113 @@
-package iterace.util
-
-import com.ibm.wala.util.graph.traverse.DFS
-import com.ibm.wala.util.graph.GraphSlicer
-import com.ibm.wala.util.graph.impl.GraphInverter
-import com.ibm.wala.ipa.callgraph.CallGraph
-import edu.illinois.wala.Facade._
-import wala.util.viz.DotUtil
-import com.ibm.wala.ipa.slicer.Slicer
-import com.ibm.wala.ipa.slicer.SDG
-import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis
-import iterace.pointeranalysis.Loop
-import iterace.pointeranalysis.Iteration
-import iterace.pointeranalysis.AlphaIteration
-import edu.illinois.wala.S
-import scala.collection.JavaConverters._
-import wala.util.viz.NodeDecorator
-import com.ibm.wala.ipa.slicer.NormalStatement
-import com.ibm.wala.ipa.slicer.Statement
-import com.ibm.wala.util.graph.Graph
-import com.ibm.wala.ipa.slicer.StatementWithInstructionIndex
-import com.ibm.wala.util.graph.GraphUtil
-import com.ibm.wala.util.graph.GraphPrint
-import iterace.datastructure.isActuallyLibraryCode
-import iterace.datastructure.isActuallyApplicationScope
-
-class Tracer(callGraph: CallGraph, pa: PointerAnalysis) {
-  print("Computing SDG... ")
-  val sdg = new SDG(callGraph, pa, Slicer.DataDependenceOptions.NO_BASE_NO_EXCEPTIONS, Slicer.ControlDependenceOptions.NONE)
-  println("DONE")
-
-  def trace(a: S[I]) = {
-    val beforeNodes = DFS.getReachableNodes(GraphInverter.invert(callGraph), Set(a.n).asJava)
-
-    //    beforeNodes.asScala foreach { n => println("n: " + n) }
-
-    val g = GraphSlicer.prune(callGraph, { n: N =>
-      n.c(Iteration) == AlphaIteration && isActuallyApplicationScope(n)
-    }) // todo: also check that it is the same loop
-
-    DotUtil.dotGraph(g, "%x".format(a.hashCode), new NodeDecorator() {
-      def getLabel(n: Object) = n.asInstanceOf[N].prettyPrint + " | " + n.hashCode
-      def getDecoration(n: Object) = ""
-      def shouldDisplay(n: Object) = true
-      def getGroup(n: Object) = ""
-    }, true)
-
-    //    val slice = Slicer.computeBackwardSlice(sdg, new NormalStatement(a.n, a.irNo))
-    //    println("DONE")
-
-    //    println(slice.size())
-    //    slice.asScala collect { case s: StatementWithInstructionIndex => s } foreach { s => println(s.prettyPrint) }
-    //
-    //    val g1 = GraphSlicer.prune(sdg, { s: Statement =>
-    //      slice.contains(s) && s.n.c(Iteration) == AlphaIteration // todo: also check that it is the same loop, see: https://github.com/cos/privatization/blob/racefix/src/test/scala/racefix/evaluation/JMolLCDsOverRaces.scala#L122
-    //    })
-    //
-    //    dotTrace(g1)
-  }
-
-  def dotHeapGraph() {
-    DotUtil.dotGraph(pa.getHeapGraph(), "heapGraph", new NodeDecorator() {
-      def getLabel(x: Object) = x match {
-        case o: O => o.prettyPrint
-        case p: P => p.prettyPrint
-        case x => x.toString()
-      }
-      def getDecoration(n: Object) = ""
-      def shouldDisplay(n: Object) = true
-      def getGroup(n: Object) = ""
-    }, true)
-  }
-
-  private def dotTrace(g: Graph[Statement], name: String = "testPlay") {
-    import com.ibm.wala.ipa.slicer.HeapStatement
-    import com.ibm.wala.ipa.slicer.HeapStatement._
-
-    DotUtil.dotGraph(g, name, new NodeDecorator() {
-      def getLabel(s: Object) = {
-        val ss = s.asInstanceOf[Statement]
-        "" + (ss match {
-          case s: StatementWithInstructionIndex =>
-            S(s.getNode(), s.getInstruction()).lineNo
-          //            S(s.getNode(), s.getInstruction()).prettyPrint // it is enclosed in the method as a cluster
-          case s: HeapStatement =>
-            (s match {
-              case s: HeapParamCaller => "" + S(s.getNode(), s.getCall()).lineNo + ":"
-              case s: HeapReturnCaller => "" + S(s.getNode(), s.getCall()).lineNo + ":"
-              case _ => ""
-            }) + s.getLocation.prettyPrint("\\n")
-
-          case s: Statement =>
-            s.getKind().toString
-        }) + "\\n"
-      }
-
-      def getDecoration(s: Object) = {
-        val nodeColor = ("%x".format(s.asInstanceOf[Statement].getNode().hashCode).substring(0, 6))
-        (s match {
-          case s: HeapParamCaller => "style = \"filled\", "
-          case s: HeapReturnCaller => "style = \"filled, dashed\", "
-          case s: HeapParamCallee => "style = \"filled, rounded\",  "
-          case s: HeapReturnCallee => "style = \"filled, rounded, dashed\", "
-          case _ => "shape=oval, style = \"filled\", "
-        }) + "margin=\"0.1,0.1\", color=\"#" + nodeColor + "\" fillcolor=\"#" + nodeColor + "25\""
-      }
-
-      def shouldDisplay(s: Object) = true
-
-      def getGroup(o: Object) = o.asInstanceOf[Statement].n.prettyPrint
-
-    }, true)
-  }
-}
+//package iterace.util
+//
+//import com.ibm.wala.util.graph.traverse.DFS
+//import com.ibm.wala.util.graph.GraphSlicer
+//import com.ibm.wala.util.graph.impl.GraphInverter
+//import com.ibm.wala.ipa.callgraph.CallGraph
+//import edu.illinois.wala.Facade._
+//import com.ibm.wala.ipa.slicer.Slicer
+//import com.ibm.wala.ipa.slicer.SDG
+//import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis
+//import iterace.pointeranalysis.Loop
+//import iterace.pointeranalysis.Iteration
+//import iterace.pointeranalysis.AlphaIteration
+//import edu.illinois.wala.S
+//import scala.collection.JavaConverters._
+//import com.ibm.wala.ipa.slicer.NormalStatement
+//import com.ibm.wala.ipa.slicer.Statement
+//import com.ibm.wala.util.graph.Graph
+//import com.ibm.wala.ipa.slicer.StatementWithInstructionIndex
+//import com.ibm.wala.util.graph.GraphUtil
+//import com.ibm.wala.util.graph.GraphPrint
+//import iterace.datastructure.isActuallyLibraryCode
+//import iterace.datastructure.isActuallyApplicationScope
+//import com.ibm.wala.viz.DotUtil
+//
+//class Tracer(callGraph: CallGraph, pa: PointerAnalysis) {
+//  print("Computing SDG... ")
+//  val sdg = new SDG(callGraph, pa, Slicer.DataDependenceOptions.NO_BASE_NO_EXCEPTIONS, Slicer.ControlDependenceOptions.NONE)
+//  println("DONE")
+//
+//  def trace(a: S[I]) = {
+//    val beforeNodes = DFS.getReachableNodes(GraphInverter.invert(callGraph), Set(a.n).asJava)
+//
+//    //    beforeNodes.asScala foreach { n => println("n: " + n) }
+//
+//    val g = GraphSlicer.prune(callGraph, { n: N =>
+//      n.c(Iteration) == AlphaIteration && isActuallyApplicationScope(n)
+//    }) // todo: also check that it is the same loop
+//
+//    DotUtil.dotGraph(g, "%x".format(a.hashCode), new NodeDecorator() {
+//      def getLabel(n: Object) = n.asInstanceOf[N].prettyPrint + " | " + n.hashCode
+//      def getDecoration(n: Object) = ""
+//      def shouldDisplay(n: Object) = true
+//      def getGroup(n: Object) = ""
+//    }, true)
+//
+//    //    val slice = Slicer.computeBackwardSlice(sdg, new NormalStatement(a.n, a.irNo))
+//    //    println("DONE")
+//
+//    //    println(slice.size())
+//    //    slice.asScala collect { case s: StatementWithInstructionIndex => s } foreach { s => println(s.prettyPrint) }
+//    //
+//    //    val g1 = GraphSlicer.prune(sdg, { s: Statement =>
+//    //      slice.contains(s) && s.n.c(Iteration) == AlphaIteration // todo: also check that it is the same loop, see: https://github.com/cos/privatization/blob/racefix/src/test/scala/racefix/evaluation/JMolLCDsOverRaces.scala#L122
+//    //    })
+//    //
+//    //    dotTrace(g1)
+//  }
+//
+//  def dotHeapGraph() {
+//    DotUtil.dotGraph(pa.getHeapGraph(), "heapGraph", new NodeDecorator() {
+//      def getLabel(x: Object) = x match {
+//        case o: O => o.prettyPrint
+//        case p: P => p.prettyPrint
+//        case x => x.toString()
+//      }
+//      def getDecoration(n: Object) = ""
+//      def shouldDisplay(n: Object) = true
+//      def getGroup(n: Object) = ""
+//    }, true)
+//  }
+//
+//  private def dotTrace(g: Graph[Statement], name: String = "testPlay") {
+//    import com.ibm.wala.ipa.slicer.HeapStatement
+//    import com.ibm.wala.ipa.slicer.HeapStatement._
+//
+//    DotUtil.dotGraph(g, name, new NodeDecorator() {
+//      def getLabel(s: Object) = {
+//        val ss = s.asInstanceOf[Statement]
+//        "" + (ss match {
+//          case s: StatementWithInstructionIndex =>
+//            S(s.getNode(), s.getInstruction()).lineNo
+//          //            S(s.getNode(), s.getInstruction()).prettyPrint // it is enclosed in the method as a cluster
+//          case s: HeapStatement =>
+//            (s match {
+//              case s: HeapParamCaller => "" + S(s.getNode(), s.getCall()).lineNo + ":"
+//              case s: HeapReturnCaller => "" + S(s.getNode(), s.getCall()).lineNo + ":"
+//              case _ => ""
+//            }) + s.getLocation.prettyPrint("\\n")
+//
+//          case s: Statement =>
+//            s.getKind().toString
+//        }) + "\\n"
+//      }
+//
+//      def getDecoration(s: Object) = {
+//        val nodeColor = ("%x".format(s.asInstanceOf[Statement].getNode().hashCode).substring(0, 6))
+//        (s match {
+//          case s: HeapParamCaller => "style = \"filled\", "
+//          case s: HeapReturnCaller => "style = \"filled, dashed\", "
+//          case s: HeapParamCallee => "style = \"filled, rounded\",  "
+//          case s: HeapReturnCallee => "style = \"filled, rounded, dashed\", "
+//          case _ => "shape=oval, style = \"filled\", "
+//        }) + "margin=\"0.1,0.1\", color=\"#" + nodeColor + "\" fillcolor=\"#" + nodeColor + "25\""
+//      }
+//
+//      def shouldDisplay(s: Object) = true
+//
+//      def getGroup(o: Object) = o.asInstanceOf[Statement].n.prettyPrint
+//
+//    }, true)
+//  }
+//}
