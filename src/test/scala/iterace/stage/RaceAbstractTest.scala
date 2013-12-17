b6a443159b11e021578534e9aa22f5fc82ce60db
yulin2 <yu.lin.86@gmail.com>
2013-12-17 13:02:16 -0600

    refactoring test interface

--- src/test/scala/iterace/stage/RaceAbstractTest.scala
+++ src/test/scala/iterace/stage/RaceAbstractTest.scala
@@ -14,6 +14,7 @@ import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigParseOptions
 import com.typesafe.config.ConfigResolveOptions
 import edu.illinois.wala.ipa.callgraph.AnalysisOptions
+import com.typesafe.config.Config
 
 abstract class RaceAbstractTest extends JavaTest {
   debug.activate
@@ -22,14 +23,14 @@ abstract class RaceAbstractTest extends JavaTest {
 
   def entryMethod = testName.getMethodName() + "()V"
 
-  def localConfig = "wala.entry { " +
+  val localConfig = (entryClass: String, entryMethod: String) => "wala.entry { " +
     "class: " + entryClass + "\n" +
     "method: " + entryMethod + " \n" +
     "}\n"
 
-  def config = ConfigFactory.parseString(localConfig) withFallback ConfigFactory.load("test")
+  val config = (localConfig: String) => ConfigFactory.parseString(localConfig) withFallback ConfigFactory.load("test")
 
-  def analysis: IteRace = IteRace(AnalysisOptions()(config), options)
+  val analysis = (config: Config) => IteRace(AnalysisOptions()(config), options)
   def options: Set[IteRaceOption]
 
   def printRaces(iterace: IteRace): String = "\n" + iterace.races.prettyPrint(
@@ -40,8 +41,12 @@ abstract class RaceAbstractTest extends JavaTest {
       }
     }) + "\n"
 
-  def expect(expectedResult: String) = assertEquals(expectedResult, printRaces(analysis))
+  def expect(entryClass: String, entryMethod: String, expectedResult: String) = {
+    assertEquals(expectedResult, printRaces(analysis(config(localConfig(entryClass, entryMethod)))))
+  }
+    
+  def expect(expectedResult: String): Unit = expect(entryClass, entryMethod, expectedResult)
   def expectNoRaces: Unit = expectNoRaces(testName.getMethodName() + "()V")
   def expectNoRaces(entry: String) = expect("\n\n")
-  def expectSomeRaces = assertNotSame("\n\n", printRaces(analysis))
+  def expectSomeRaces = assertNotSame("\n\n", printRaces(analysis(config(localConfig(entryClass, entryMethod)))))
 }
